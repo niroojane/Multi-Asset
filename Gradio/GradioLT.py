b491ce464bdd72f5360ec9ff78abb82c98718bf6
niroojane <116459392+niroojane@users.noreply.github.com>
2025-10-16 12:05:36 +0100

    Add files via upload

---
+++ Gradio/GradioLT.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+import gradio as gr
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import os
+from scipy.optimize import minimize
+from RiskMetrics import RiskAnalysis, diversification_constraint, create_constraint
+from Rebalancing import rebalanced_portfolio, buy_and_hold
+
+
+def objective(w):
+    return np.sqrt(np.sum((w - w0) ** 2))
+
+def sum_equal_one(weight):
+    return np.sum(weight) - 1
+
+
+drop_down_list = []
+constraints_options = []
+constraints=[]
+data = pd.DataFrame(columns=["Asset", "Sign", "Limit"])
+res = pd.DataFrame()
+
+def load_excel(file):
+    global full_matrix, full_matrix_numpy, w0, drop_down_list, drop_down_list_sector, drop_down_list_asset, constraints_options,bounds_sectors_dataframe
+
+    try:
+        file = pd.ExcelFile(file.name)
+        data_file = file.parse(sheet_name=file.sheet_names)
+
+        holdings = data_file['Holdings'].set_index('Name')
+        holdings = holdings.loc[holdings.index != 'Cash EUR']
+        holdings['Portfolio Weighting %'] = holdings['Portfolio Weighting %'] / holdings['Portfolio Weighting %'].sum()
+
+        sheets = file.sheet_names
+        sheets.remove('Holdings')
+        transparency = {}
+
+        for sheet in sheets:
+            temp = data_file[sheet].set_index('Name').iloc[:, 1:]
+            temp = temp.loc[temp.index != 'Cash EUR']
+            temp = temp.loc[holdings.index]
+            transparency[sheet] = temp / 100
+
+        full_matrix = pd.DataFrame()
+        for key in transparency:
+            full_matrix = pd.concat([full_matrix, transparency[key]], axis=1)
+
+        full_matrix_numpy = full_matrix.to_numpy()
+        w0 = holdings['Portfolio Weighting %'].loc[full_matrix.index].to_numpy()
+
+        drop_down_list_asset = list(full_matrix.index) + ['All']
+        drop_down_list_sector = list(full_matrix.columns)
+        drop_down_list = drop_down_list_asset + drop_down_list_sector + [None]
+        constraints_options = ["=", "≥", "≤"]
+        bounds_sectors={}
+
+        for col in full_matrix.columns:
+            min_bounds=round(full_matrix[col].min(),4)
+            max_bounds=round(full_matrix[col].max(),4)
+            name_max=full_matrix[col].idxmax()
+            name_min=full_matrix[col].idxmin()
+
+            bounds_sectors[col]=[min_bounds,max_bounds,name_min,name_max]
+
+        bounds_sectors_dataframe=pd.DataFrame(bounds_sectors,index=['Lower Bound','Upper Bound','Name Min','Name Max']).T.round(4).reset_index().rename(columns={'index': 'Sectors'})
+        
+        return "File uploaded successfully!",gr.update(choices=drop_down_list)
+
+    except Exception as e:
+        return f"Error: {str(e)}", gr.update(choices=[])
+
+def transparency_matrices():
+
+    return full_matrix.reset_index().rename(columns={'index': 'Asset'}).round(4),bounds_sectors_dataframe
+
+
+        
+def submit(value1, value2, value3):
+    global data, constraints
+
+    new_row = {"Asset": value1, "Sign": value2, "Limit": value3}
+    data = pd.concat([data, pd.DataFrame([new_row])], ignore_index=True)
+
+    constraint_matrix = pd.DataFrame(data).to_numpy()
+    
+    constraints = [{'type': 'eq', 'fun': sum_equal_one}]
+    
+    dico_map = {'=': 'eq', '≥': 'ineq', '≤': 'ineq'}
+    
+    try:
+        for row in range(constraint_matrix.shape[0]):
+            temp = constraint_matrix[row, :]
+            ticker = temp[0]
+
+            if ticker not in drop_down_list:
+                continue
+
+            sign = temp[1]
+            limit = float(temp[2])
+
+            if ticker == 'All':
+                constraint = diversification_constraint(sign, limit)
+
+            elif ticker in drop_down_list_asset:
+                position = np.where(full_matrix.index == ticker)[0][0]
+                constraint = create_constraint(sign, limit, position)
+
+            elif ticker in drop_down_list_sector:
+                position = np.where(full_matrix.columns == ticker)[0][0]
+                if sign == '≤':
+                    constraint = [{'type': dico_map[sign], 'fun': lambda weights: limit - (weights @ full_matrix_numpy)[position]}]
+                elif sign == '≥':
+                    constraint = [{'type': dico_map[sign], 'fun': lambda weights: (weights @ full_matrix_numpy)[position] - limit}]
+                else:
+                    constraint = [{'type': dico_map[sign], 'fun': lambda weights: (weights @ full_matrix_numpy)[position] - limit}]
+
+            constraints.extend(constraint)
+
+    except Exception as e:
+        pass
+
+    return data
+
+def reset_constraints():
+    global data, constraints
+    data = pd.DataFrame(columns=["Asset", "Sign", "Limit"])
+    constraints = []
+    return data
+
+def add_allocation(new_allocation):
+    
+    global opt_weights,res
+    
+    bounds = [(0, 1) for _ in range(full_matrix.shape[0])]
+
+    if constraints:
+        result = minimize(objective, w0, method="SLSQP", bounds=bounds, constraints=constraints)
+    else:
+        result = minimize(objective, w0, method="SLSQP", bounds=bounds, constraints=[])
+        
+    opt_weights = result.x
+    
+    initial = pd.DataFrame(w0, index=full_matrix.index, columns=["Initial"])
+    optimal = pd.DataFrame(opt_weights, index=full_matrix.index, columns=["Optimised"])
+    
+    if not new_allocation or len(new_allocation.strip()) == 0:
+        new_allocation = '1.0'+',0.0' * (w0.shape[0] - 1) 
+
+    try:
+        new_allocation = [float(x) for x in new_allocation.split(',')]
+
+        if len(new_allocation) != w0.shape[0]:
+            return "Error: Number of allocation values does not match the number of assets.", res
+
+        total = sum(new_allocation)
+        if total != 0:
+            new_allocation = [x / total for x in new_allocation]
+
+        
+        new_allocation_weights=pd.DataFrame(new_allocation, index=full_matrix.index, columns=["Custom Allocation"])
+        res = pd.concat([initial, optimal,new_allocation_weights], axis=1)    
+        
+        change=res.copy()
+
+        for col in change.columns:
+    
+            change[col]=res[col]-res['Initial']
+
+        return res.reset_index().rename(columns={'index': 'Asset'}).round(4), change.reset_index().rename(columns={'index': 'Asset'}).round(4),(res.T@full_matrix).T.reset_index().rename(columns={'index': 'Sectors'}).round(4),(change.T@full_matrix).T.reset_index().rename(columns={'index': 'Sectors'}).round(4)
+        
+    except Exception as e:
+
+        res = pd.concat([initial, optimal], axis=1)    
+    
+        change=res.copy()
+
+        for col in change.columns:
+    
+            change[col]=res[col]-res['Initial']
+
+        return res.reset_index().rename(columns={'index': 'Asset'}).round(4),change.reset_index().rename(columns={'index': 'Asset'}).round(4),(res.T@full_matrix).T.reset_index().rename(columns={'index': 'Sectors'}).round(4),(change.T@full_matrix).T.reset_index().rename(columns={'index': 'Sectors'}).round(4)
+        
+def clear_allocation():
+    
+    global res
+        
+    initial = pd.DataFrame(w0, index=full_matrix.index, columns=["Initial"])
+    optimal = pd.DataFrame(opt_weights, index=full_matrix.index, columns=["Optimised"])
+    
+    res = pd.concat([initial, optimal], axis=1)
+    
+    change=res.copy()
+
+    for col in change.columns:
+
+        change[col]=res[col]-res['Initial']
+
+    return res.reset_index().rename(columns={'index': 'Asset'}).round(4), change.reset_index().rename(columns={'index': 'Asset'}).round(4),(res.T@full_matrix).T.reset_index().rename(columns={'index': 'Sectors'}).round(4),(change.T@full_matrix).T.reset_index().rename(columns={'index': 'Sectors'}).round(4)
+
+
+with gr.Blocks(css="* { font-family: 'Arial Narrow', sans-serif; }") as app:    
+
+    with gr.Tab("Rebalancing Optimizer"):
+        gr.Markdown("## Rebalancing Optimizer")
+    
+        # Define UI elements first
+        file_input = gr.File(label="Upload Excel File (.xlsx)")
+        file_status = gr.Textbox(label="Status", interactive=False)
+        asset_dropdown = gr.Dropdown(choices=drop_down_list, label="Asset or Sector")  # << Move this up
+        sign_dropdown = gr.Dropdown(choices=["=", "≥", "≤"], label="Sign")
+        limit_input = gr.Number(label="Limit (Float)")
+    
+        file_input.change(load_excel, inputs=file_input, outputs=[file_status, asset_dropdown])
+    
+        # Button to submit constraint
+        submit_button = gr.Button("Add Constraint")
+        reset_button = gr.Button("Reset Constraints")
+        constraints_table = gr.Dataframe(headers=["Asset", "Sign", "Limit"], interactive=False)
+        
+        submit_button.click(
+            submit,
+            inputs=[asset_dropdown, sign_dropdown, limit_input],
+            outputs=[constraints_table]
+        )
+
+
+        reset_button.click(
+            reset_constraints,
+            outputs=[constraints_table]
+        )
+
+        
+        # Optimize
+
+        optimize_button = gr.Button("Optimize Portfolio")
+        clear_allocation_button = gr.Button("Reset Allocation")
+        new_allocation_input = gr.Textbox(label="Enter Allocation (comma separated)")
+
+        weights_output = gr.Dataframe(label="Optimized Weights")
+        delta_output = gr.Dataframe(label="Variation of weights")
+        exposure_output = gr.Dataframe(label="Optimized Sector Exposure")
+        change_output=gr.Dataframe(label='Sector Change')
+        # optimize_button.click(optimize, outputs=[weights_output,exposure_output])
+
+        
+        clear_allocation_button.click(fn=clear_allocation, inputs=[], outputs=[weights_output,delta_output,exposure_output,change_output])
+        new_allocation_input.submit(fn=add_allocation, inputs=new_allocation_input, outputs=[weights_output,delta_output,exposure_output,change_output])
+        optimize_button.click(fn=add_allocation, inputs=new_allocation_input, outputs=[weights_output,delta_output,exposure_output,change_output])
+        
+
+    with gr.Tab("Transparency Matrix"):
+        
+        transparency_matrix = gr.Dataframe(headers=["Sectors", "Lower Bound", "Upper Bound"], interactive=False)
+        bound_matrix = gr.Dataframe(label="Sectors Bounds")
+        
+        show_bounds = gr.Button("Underlying Exposure")
+
+        show_bounds.click(transparency_matrices, outputs=[transparency_matrix,bound_matrix])
+
+port=7860
+app.launch(debug=True, share=False, server_port=port, server_name='0.0.0.0', root_path=f'/studio/vscode/{os.getenv("ALTO_STUDIO_USERNAME")}/proxy/{port}/')
+        
+
+# In[ ]:
+
+
+
+
+
+
+
